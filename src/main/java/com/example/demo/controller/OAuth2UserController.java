0656710d512581c8470ab1c87166bff5675b79e6
DESKTOP-5QB9KIT\jks55 <jkyoung37@gmail.com>
2021-03-14 17:52:56 +0900

    【成功】 home, login, board

---
+++ src/main/java/com/example/demo/controller/OAuth2UserController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OAuth2UserController {
+  @GetMapping({"", "/"})
+  public String getAuthorizationMessage() {
+    return "home";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
+
+  @GetMapping({"/loginSuccess", "/board"})
+  public String loginSuccess() {
+    return "board";
+  }
+}
